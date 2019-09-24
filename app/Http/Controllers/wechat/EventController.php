f960e58603ade9c682cd618b7115a5d890b6e7c0
kailaixin <1124802717@qq.com>
2019-09-24 17:37:01 +0800

    'aa\'

--- app/Http/Controllers/wechat/EventController.php
+++ app/Http/Controllers/wechat/EventController.php
@@ -32,12 +32,26 @@ class EventController extends Controller
         \Log::Info(json_encode($xml_arr,JSON_UNESCAPED_UNICODE));
 //        echo $_GET['echostr'];
         //业务逻辑部分
+                //签到逻辑
+//        if($xml_arr['MsgType'] == 'event' && $xml_arr['Event'] == 'CLICK'){
+//
+//        }
 //            关注公众号的逻辑
         if($xml_arr['MsgType'] == 'event' && $xml_arr['Event'] == 'subscribe'){
             //拿到用户基本信息的openid
             $user_url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$this->tools->get_wechat_access_token().'&openid='.$xml_arr['FromUserName'].'&lang=zh_CN';
             $res = file_get_contents($user_url);
             $res = json_decode($res,1);
+            //将关注的用户存入数据库中
+            $user_info = DB::connection('1901')->table('wechat_openid')->where(['openid'=>$xml_arr['FromUserName']])->first();
+//            dd($user_info);
+            if(empty($user_info)){
+                //m没有数据存入
+                DB::connection('1901')->table('wechat_openid')->insert([
+                        'openid'=>$xml_arr['FromUserName'],
+                        'add_time'=>time(),
+                ]);
+            }
 
             $message = '欢迎'.$res['nickname'].'同学，感谢您的关注';
             $xml_str = '<xml><ToUserName><![CDATA['.$xml_arr['FromUserName'].']]></ToUserName><FromUserName><![CDATA['.$xml_arr['ToUserName'].']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['.$message.']]></Content></xml>';
