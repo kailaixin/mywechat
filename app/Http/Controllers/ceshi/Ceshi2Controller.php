0fc5da698050ad3853bf26c933d2df6dfb602b98
kailaixin <1124802717@qq.com>
2019-09-24 11:07:40 +0800

    '13'

--- app/Http/Controllers/ceshi/Ceshi2Controller.php
+++ app/Http/Controllers/ceshi/Ceshi2Controller.php
@@ -18,6 +18,7 @@ class Ceshi2Controller extends Controller
     //添加自定义菜单
     public function create_menu()
     {
+        dd(date('Y-m-d H:i:s' ,1569286578));
         return view('ceshi/create_menu');
     }
     //自定义菜单处理
@@ -104,45 +105,4 @@ class Ceshi2Controller extends Controller
         dd($result);
     }
 
-    //关注,取消关注事件
-    public function event()
-    {
-//        echo $_GET['echostr'];die();
-//        dd($_POST);
-        $xml_string = file_get_contents('php://input'); // 获取微信发过来的xml数据
-//        dd($xml_string);
-        $wechat_log_path = storage_path('/logs/wechat/'.date("Y-m-d").'.log');  // 生成日志文件
-        file_put_contents($wechat_log_path,"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n",FILE_APPEND);
-        file_put_contents($wechat_log_path,$xml_string,FILE_APPEND);
-        file_put_contents($wechat_log_path,"\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n\n",FILE_APPEND);
-
-//        dd($xml_string);
-        $xml_obj = simplexml_load_string($xml_string,'SimpleXMLElement',LIBXML_NOCDATA);
-        $xml_arr = (array)$xml_obj;
-//        dd($xml_arr);
-        \Log::Info(json_encode($xml_arr,JSON_UNESCAPED_UNICODE));
-//        echo $_GET['echostr'];
-
-
-        // 业务逻辑（防止刷业务）
-        if ($xml_arr['MsgType'] == 'event') {
-            if ($xml_arr['Event'] == 'subscribe') {
-                $share_code = explode('_',$xml_arr['EventKey'])[1];
-                $user_openid = $xml_arr['FromUserName']; // 粉丝的openid
-                // 判断是否已经在日志里
-                $wechat_openid = DB::table('wechat_openid')->where('openid',$user_openid)->first();
-                if (empty($wechat_openid)) {
-                    DB::table('users')->where('id',$share_code)->increment('share_num',1);
-                    DB::table('wechat_openid')->insert([
-                        'openid' => $user_openid,
-                        'add_time' => time()
-                    ]);
-                }
-            }
-        }
-
-        $message = '欢迎关注,不要走开,精彩继续';
-        $xml_str = '<xml><ToUserName><![CDATA['.$xml_arr['FromUserName'].']]></ToUserName><FromUserName><![CDATA['.$xml_arr['ToUserName'].']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['.$message.']]></Content></xml>';
-        echo $xml_str;
-    }
 }
--- app/Http/Controllers/wechat/EventController.php
+++ app/Http/Controllers/wechat/EventController.php
@@ -13,7 +13,7 @@ class EventController extends Controller
 //        echo $_GET['echostr'];die();
 //        dd($_POST);
         $xml_string = file_get_contents('php://input'); // 获取微信发过来的xml数据
-        dd($xml_string);
+//        dd($xml_string);
         $wechat_log_path = storage_path('/logs/wechat/'.date("Y-m-d").'.log');  // 生成日志文件
         file_put_contents($wechat_log_path,"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n",FILE_APPEND);
         file_put_contents($wechat_log_path,$xml_string,FILE_APPEND);
@@ -26,9 +26,26 @@ class EventController extends Controller
         \Log::Info(json_encode($xml_arr,JSON_UNESCAPED_UNICODE));
 //        echo $_GET['echostr'];
 
-
+        // 业务逻辑（防止刷业务）
+        if ($xml_arr['MsgType'] == 'event') {
+            if ($xml_arr['Event'] == 'subscribe') {
+                $share_code = explode('_',$xml_arr['EventKey'])[1];
+                $user_openid = $xml_arr['FromUserName']; // 粉丝的openid
+                // 判断是否已经在日志里
+                $wechat_openid = DB::table('wechat_openid')->where('openid',$user_openid)->first();
+                if (empty($wechat_openid)) {
+                    DB::table('users')->where('id',$share_code)->increment('share_num',1);
+                    DB::table('wechat_openid')->insert([
+                        'openid' => $user_openid,
+                        'add_time' => time()
+                    ]);
+                }
+            }
+        }
         $message = '小生不才,未得姑娘青睐,扰姑娘良久,姑娘勿怪！';
         $xml_str = '<xml><ToUserName><![CDATA['.$xml_arr['FromUserName'].']]></ToUserName><FromUserName><![CDATA['.$xml_arr['ToUserName'].']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['.$message.']]></Content></xml>';
+//        $xml_str = '<xml><ToUserName><![CDATA['.$xml_arr['FromUserName'].']]></ToUserName><FromUserName><![CDATA['.$xml_arr['ToUserName'].']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['.$message.']]></Content></xml>';
+
         echo $xml_str;
     }
 }
