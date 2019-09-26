da5adc0d76b7422c208bb53289b4ff48acc71531
kailaixin <1124802717@qq.com>
2019-09-26 09:14:09 +0800

    '334'

--- app/Http/Controllers/wechat/EventController.php
+++ app/Http/Controllers/wechat/EventController.php
@@ -35,7 +35,7 @@ class EventController extends Controller
 //             管理课程逻辑部分
             if($xml_arr['MsgType'] == 'event' && $xml_arr['Event'] == 'CLICK'){
                    if ($xml_arr['EventKey'] == 'guanli'){//管理课程页面
-                       header('Location:59.110.216.249/ceshi3/guanli');
+                       return redirect('/ceshi3/guanli');
                    }
                    if($xml_arr['EventKey'] == 'chakan'){//查看课程页面
 
